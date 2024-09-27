0983981d98dff0ecfe9473de93de43ca1f00c176
Eduard Konovka <ed098ua@gmail.com>
2024-09-27 23:50:21 +0300

    Fixes computedHeight

--- src/App.js
+++ src/App.js
@@ -65,19 +65,19 @@ export default function App() {
     // Container, header and footer subtracted from viewport height
     const computedHeight =
       appWidth < 320
-        ? appHeight - (10 + (appWidth / 1.124 + 5) + 43)
+        ? appHeight - (10 + (appWidth * 0.265 + appWidth * 0.6 + 5) + 43)
         : appWidth < 420
-        ? appHeight - (12 + (appWidth / 1.387 + 6) + 47)
+        ? appHeight - (12 + (88 + 182 + 6) + 47)
         : appWidth < 800
-        ? appHeight - (14 + (appWidth / 4 + 7) + 50)
+        ? appHeight - (14 + (78 + 42 + 7) + 50)
         : appWidth < 1024
-        ? appHeight - (16 + (appWidth / 6.25 + 8) + 52)
+        ? appHeight - (16 + (98 + 46 + 8) + 52)
         : appWidth < 1600
-        ? appHeight - (20 + (appWidth / 7.238 + 10) + 57)
+        ? appHeight - (20 + (142 + 60 + 10) + 57)
         : appHeight -
           (appWidth / 80 +
-            (appWidth / 7.813 + appWidth / 160) +
-            (appWidth / 53.333 + appWidth / 55.56));
+            (appWidth * 0.0885 + appWidth * 0.04 + appWidth / 160) +
+            (appWidth * 0.01875 + appWidth / 55.56));
 
     changeGlobalState(updateMainHeight, computedHeight);
     // eslint-disable-next-line react-hooks/exhaustive-deps
