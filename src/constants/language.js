a61b184559bc4fed8dbb26bd4786c3044aa346b6
Eduard Konovka <ed098ua@gmail.com>
2024-10-02 17:38:40 +0300

    Adds Russian

--- src/constants/language.js
+++ src/constants/language.js
@@ -2,57 +2,68 @@ export const LANGUAGE = {
   errorStoreLanguage: {
     eng: 'Error writing localStorage language data: ',
     ukr: 'Помилка запису мовних даних localStorage: ',
+    rus: '',
   },
 
   errorInfo: {
     eng: 'Information about this error: ',
     ukr: 'Інформація про цю помилку: ',
+    rus: '',
   },
 
   errorBoundary: {
     eng: 'Oops! Something went wrong...',
     ukr: 'Ой! Щось пішло не так...',
+    rus: '',
   },
 
   logoAlt: {
     eng: 'Logotype',
     ukr: 'Логотип',
+    rus: '',
   },
 
   titles: {
     brand: {
       eng: 'Cake decorations',
       ukr: 'Тортові прикраси',
+      rus: '',
     },
 
     categories: {
       eng: 'Categories',
       ukr: 'Категорії',
+      rus: '',
     },
 
     products: {
       eng: 'Products',
       ukr: 'Товари',
+      rus: '',
     },
 
     about: {
       eng: 'About us',
       ukr: 'Про нас',
+      rus: '',
     },
 
     contacts: {
       eng: 'Contacts',
       ukr: 'Контакти',
+      rus: '',
     },
 
     delivery: {
       eng: 'Delivery and payment',
       ukr: 'Доставка та оплата',
+      rus: '',
     },
 
     portfolio: {
       eng: 'Portfolio',
       ukr: 'Портфоліо',
+      rus: '',
     },
   },
 
@@ -60,11 +71,13 @@ export const LANGUAGE = {
     productDuplication: {
       eng: 'This item is already in the cart!',
       ukr: 'Цей товар вже в кошику!',
+      rus: '',
     },
 
     productAdded: {
       eng: 'The product has been successfully added to the cart!',
       ukr: 'Товар успішно додано в кошик!',
+      rus: '',
     },
   },
 
@@ -72,112 +85,133 @@ export const LANGUAGE = {
     categoriesLink: {
       eng: 'Go to the product categories page',
       ukr: 'Перейти на сторінку категорій товарів',
+      rus: '',
     },
 
     productsLink: {
       eng: 'Go to product list',
       ukr: 'Перейти до списку товарів',
+      rus: '',
     },
 
     aboutLink: {
       eng: 'Go to the page about us',
       ukr: 'Перейти на сторінку про нас',
+      rus: '',
     },
 
     contactsLink: {
       eng: 'Go to our contacts page',
       ukr: 'Перейти на сторінку наших контактів',
+      rus: '',
     },
 
     deliveryLink: {
       eng: 'Go to the shipping and payment information page',
       ukr: 'Перейти на сторінку інформації про доставку та оплату',
+      rus: '',
     },
 
     portfolioLink: {
       eng: 'Go to our portfolio',
       ukr: 'Перейти до нашого портфоліо',
+      rus: '',
     },
 
     cartLink: {
       eng: 'Go to cart',
       ukr: 'Перейти до кошика',
+      rus: '',
     },
 
     signOut: {
       title: {
         eng: 'Signing out of your account',
         ukr: 'Вихід з облікового запису',
+        rus: '',
       },
 
       text: {
         eng: 'Sign out',
         ukr: 'Вийти з аккаунта',
+        rus: '',
       },
     },
 
     avatarAlt: {
       eng: 'User avatar',
       ukr: 'Аватар користувача',
+      rus: '',
     },
 
     hello: {
       eng: 'Hello, guest!',
       ukr: 'Привіт, гість!',
+      rus: '',
     },
 
     language: {
       eng: 'Select your language',
       ukr: 'Виберіть свою мову',
+      rus: '',
     },
   },
 
   notFoundView: {
     eng: 'Check the correctness of the entered in the address bar',
     ukr: 'Перевірте правильність введеного в адресному рядку',
+    rus: '',
   },
 
   order: {
     status: {
       eng: 'Status: ',
       ukr: 'Статус: ',
+      rus: '',
     },
 
     success: {
       eng: '. Cart contents successfully accepted for processing. Thanks for your order!',
       ukr: '. Вміст кошика успішно прийнято для обробки. Дякую за ваше замовлення!',
+      rus: '',
     },
 
     error: {
       eng: 'Failed to process order! Error: ',
       ukr: 'Не вдалося обробити замовлення! Помилка:',
+      rus: '',
     },
   },
 
   viewError: {
     eng: 'Whoops, something went wrong: ',
     ukr: 'На жаль, щось пішло не так: ',
+    rus: '',
   },
 
   openShopAlt: {
     eng: 'Open shop',
     ukr: 'Відкритий магазин',
+    rus: '',
   },
 
   noProducts: {
     eng: 'At the moment, there are no products for sale in the store',
     ukr: 'На даний момент в магазині немає товарів у продажу',
+    rus: '',
   },
 
   searchByName: {
     title: {
       eng: 'Search by product name',
       ukr: 'Шукати за назвою товару',
+      rus: '',
     },
 
     error: {
       eng: 'Enter the correct product name!',
       ukr: 'Введіть правильну назву товару!',
+      rus: '',
     },
   },
 
@@ -185,11 +219,13 @@ export const LANGUAGE = {
     title: {
       eng: 'Reset all filters',
       ukr: 'Скинути всі фільтри',
+      rus: '',
     },
 
     text: {
       eng: 'Reset filters',
       ukr: 'Скинути фільтри',
+      rus: '',
     },
   },
 
@@ -197,26 +233,31 @@ export const LANGUAGE = {
     label: {
       eng: 'Sort',
       ukr: 'Сортувати',
+      rus: '',
     },
 
     ascendingCode: {
       eng: 'by ascending barcode',
       ukr: 'за зростанням штрих-коду',
+      rus: '',
     },
 
     descendingCode: {
       eng: 'by descending barcode',
       ukr: 'за зниженням штрих-коду',
+      rus: '',
     },
 
     ascendingPrice: {
       eng: 'from cheap to expensive',
       ukr: 'від дешевого до дорогого',
+      rus: '',
     },
 
     descendingPrice: {
       eng: 'from expensive to cheap',
       ukr: 'від дорогого до дешевого',
+      rus: '',
     },
   },
 
@@ -224,11 +265,13 @@ export const LANGUAGE = {
     title: {
       eng: 'Your cart is empty',
       ukr: 'Ваш кошик порожній',
+      rus: '',
     },
 
     emptyCartAlt: {
       eng: 'Empty cart',
       ukr: 'Порожній кошик',
+      rus: '',
     },
   },
 
@@ -236,26 +279,31 @@ export const LANGUAGE = {
     username: {
       eng: 'Username:',
       ukr: 'Ім’я користувача:',
+      rus: '',
     },
 
     inputTitle: {
       eng: 'The length of the name must not be less than 4 and more than 16 characters. The name can only consist of letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d’Artagnan, etc.',
       ukr: 'Довжина імені не повинна бути менше 4 і більше 16 символів. Ім’я може складатися лише з букв, апострофа, тире та пробілів. Наприклад, Адріан, Якоб Мерсер, Шарль де Бац де Кастельмор д’Артаньян тощо.',
+      rus: '',
     },
 
     inputPlaceholder: {
       eng: 'Enter your name...',
       ukr: 'Введіть ім’я...',
+      rus: '',
     },
 
     buttonTitle: {
       eng: 'Sign in to your account',
       ukr: 'Увійдіть у свій обліковий запис',
+      rus: '',
     },
 
     buttonText: {
       eng: 'Sign in',
       ukr: 'Увійти',
+      rus: '',
     },
   },
 
@@ -263,41 +311,49 @@ export const LANGUAGE = {
     product_type: {
       eng: 'Category: ',
       ukr: 'Категорія: ',
+      rus: '',
     },
 
     tags: {
       eng: 'Tags: ',
       ukr: 'Теги:',
+      rus: '',
     },
 
     links: {
       eng: 'What is ',
       ukr: 'Що таке ',
+      rus: '',
     },
 
     price: {
       eng: 'Price: ',
       ukr: 'Ціна: ',
+      rus: '',
     },
 
     сollapseButtonTitle: {
       eng: 'Collapse',
       ukr: 'Згорнути',
+      rus: '',
     },
 
     up: {
       eng: 'To the top of the page',
       ukr: 'До початку сторінки',
+      rus: '',
     },
 
     right: {
       eng: 'Next image',
       ukr: 'Наступне зображення',
+      rus: '',
     },
 
     left: {
       eng: 'Previous image',
       ukr: 'Попереднє зображення',
+      rus: '',
     },
   },
 
@@ -305,27 +361,32 @@ export const LANGUAGE = {
     product_type: {
       eng: 'Category: ',
       ukr: 'Категорія: ',
+      rus: '',
     },
 
     barcode: {
       eng: 'Barcode: ',
       ukr: 'Штрих-код: ',
+      rus: '',
     },
 
     price: {
       eng: 'Price: ',
       ukr: 'Ціна: ',
+      rus: '',
     },
 
     button: {
       title: {
         eng: 'Add the product to the cart',
         ukr: 'Додайте товар у кошик',
+        rus: '',
       },
 
       text: {
         eng: 'Add to cart',
         ukr: 'В кошик',
+        rus: '',
       },
     },
   },
@@ -334,16 +395,19 @@ export const LANGUAGE = {
     totalCost: {
       eng: 'Total cost: ',
       ukr: 'Загальна вартість: ',
+      rus: '',
     },
 
     buttonTitle: {
       eng: 'Send order for clearance',
       ukr: 'Відправити замовлення на оформлення',
+      rus: '',
     },
 
     buttonText: {
       eng: 'Purchase',
       ukr: 'Оформлення',
+      rus: '',
     },
   },
 
@@ -351,27 +415,32 @@ export const LANGUAGE = {
     label: {
       eng: 'Count, units:',
       ukr: 'Кількість:',
+      rus: '',
     },
 
     totalPrice: {
       eng: 'Total price:',
       ukr: 'Загальна сума:',
+      rus: '',
     },
 
     error: {
       prefix: {
         eng: 'Please enter an integer value from',
         ukr: 'Будь ласка, введіть ціле число від',
+        rus: '',
       },
 
       suffix: {
         eng: 'to',
         ukr: 'до',
+        rus: '',
       },
 
       postfix: {
         eng: 'inclusive!',
         ukr: 'включно!',
+        rus: '',
       },
     },
   },
@@ -379,32 +448,38 @@ export const LANGUAGE = {
   links: {
     eng: 'Look it up on Google',
     ukr: 'Шукати в Google',
+    rus: '',
   },
 
   tags: {
     eng: 'Browse products tagged',
     ukr: 'Перегляньте продукти з тегом',
+    rus: '',
   },
 
   optionList: {
     all: {
       eng: 'All prices',
       ukr: 'Всі ціни',
+      rus: '',
     },
 
     down: {
       eng: 'Prices <',
       ukr: 'Менше за',
+      rus: '',
     },
 
     middle: {
       eng: '< prices <',
       ukr: '< ціна <',
+      rus: '',
     },
 
     up: {
       eng: 'Prices >',
       ukr: 'Більше за',
+      rus: '',
     },
   },
 
@@ -412,16 +487,19 @@ export const LANGUAGE = {
     title: {
       eng: 'Your order has been sent for processing',
       ukr: 'Ваше замовлення відправлено в обробку',
+      rus: '',
     },
 
     gearsAlt: {
       eng: 'Gears',
       ukr: 'Шестерні',
+      rus: '',
     },
 
     processingAlt: {
       eng: 'Order processing',
       ukr: 'Обробка замовлення',
+      rus: '',
     },
   },
 
@@ -429,43 +507,51 @@ export const LANGUAGE = {
     titleLink: {
       eng: 'Go to the product',
       ukr: 'Перейти до товару',
+      rus: '',
     },
 
     price: {
       eng: 'Price: ',
       ukr: 'Ціна: ',
+      rus: '',
     },
 
     buttonTitle: {
       eng: 'Remove product from cart',
       ukr: 'Видалити товар з кошика',
+      rus: '',
     },
 
     buttonText: {
       eng: 'Delete',
       ukr: 'Видалити',
+      rus: '',
     },
   },
 
   spinnerAlt: {
     eng: 'Spinner',
     ukr: 'Спіннер',
+    rus: '',
   },
 
   footer: {
     label: {
       eng: 'Created by',
       ukr: 'Творець сайту:',
+      rus: '',
     },
 
     title: {
       eng: 'Go to the resume site of Eduard Konovka',
       ukr: 'Перейти на сайт-резюме Едуарда Коновки',
+      rus: '',
     },
 
     name: {
       eng: 'Eduard Konovka',
       ukr: 'Едуард Коновка',
+      rus: '',
     },
   },
 };
