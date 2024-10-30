72052ab6629f830543571ba99c5412f54be0ded9
Eduard Konovka <ed098ua@gmail.com>
2024-10-30 19:28:38 +0200

    Fixes CursorInteraction structure

--- src/components/CursorInteraction/CursorInteraction.jsx
+++ src/components/CursorInteraction/CursorInteraction.jsx
@@ -5,7 +5,7 @@ import s from './CursorInteraction.module.css';
 
 const CANVAS_ID = 'canvas';
 
-export default function CursorInteraction({ magicBoxSize = 100, children }) {
+export default function CursorInteraction({ children }) {
   const [canvas, setCanvas] = useState(null);
   const [staticCoords, setStaticCoords] = useState(true);
   const [xCoordinate, setXCoordinate] = useState(null);
@@ -42,43 +42,33 @@ export default function CursorInteraction({ magicBoxSize = 100, children }) {
     <div id={CANVAS_ID} className={s.canvas}>
       {children}
 
-      <div
-        className={staticCoords ? s.activMagicBox : s.magicBox}
-        style={{
-          width: magicBoxSize,
-          height: magicBoxSize,
-          top: yCoordinate - magicBoxSize / 2,
-          left: xCoordinate - magicBoxSize / 2,
-        }}
-      >
-        <img
-          src={star}
-          alt="star"
-          className={s.star}
-          style={{ top: 0, left: 0 }}
-        />
+      <img
+        src={star}
+        alt="star"
+        className={staticCoords ? s.activStar : s.star}
+        style={{ top: yCoordinate - 60, left: xCoordinate - 60 }}
+      />
 
-        <img
-          src={star}
-          alt="star"
-          className={s.star}
-          style={{ top: 0, right: 0 }}
-        />
+      <img
+        src={star}
+        alt="star"
+        className={staticCoords ? s.activStar : s.star}
+        style={{ top: yCoordinate - 60, left: xCoordinate + 30 }}
+      />
 
-        <img
-          src={star}
-          alt="star"
-          className={s.star}
-          style={{ bottom: 0, left: 0 }}
-        />
+      <img
+        src={star}
+        alt="star"
+        className={staticCoords ? s.activStar : s.star}
+        style={{ top: yCoordinate + 30, left: xCoordinate - 60 }}
+      />
 
-        <img
-          src={star}
-          alt="star"
-          className={s.star}
-          style={{ bottom: 0, right: 0 }}
-        />
-      </div>
+      <img
+        src={star}
+        alt="star"
+        className={staticCoords ? s.activStar : s.star}
+        style={{ top: yCoordinate + 30, left: xCoordinate + 30 }}
+      />
     </div>
   );
 }
