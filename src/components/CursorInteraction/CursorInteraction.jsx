f48dee0de2a5080e58afc36a6b9b41058c4832d5
Eduard Konovka <ed098ua@gmail.com>
2024-11-01 13:25:33 +0200

    Creates a circular motion CursorInteraction

--- src/components/CursorInteraction/CursorInteraction.jsx
+++ src/components/CursorInteraction/CursorInteraction.jsx
@@ -4,17 +4,18 @@ import star from 'assets/star.png';
 import s from './CursorInteraction.module.css';
 
 const CANVAS_ID = 'canvas';
-let yCount = 0;
-// TODO xCount
+const CENTERING = -15;
+const DIAMETER = 90;
 
-export default function CursorInteraction({ children }) {
+export default function CursorInteraction({ starQuantity = 15, children }) {
   const [canvas, setCanvas] = useState(null);
   const [staticCoords, setStaticCoords] = useState(true);
+  const [starOffsets, setStarOffsets] = useState([]);
   const [pageX, setPageX] = useState(null);
   const [pageY, setPageY] = useState(null);
   const [xCoordinate, setXCoordinate] = useState(null);
   const [yCoordinate, setYCoordinate] = useState(null);
-  const [yOffset, setYOffset] = useState(0);
+  const [torque, setTorque] = useState(0);
 
   useEffect(() => {
     setCanvas(document.getElementById(CANVAS_ID));
@@ -33,7 +34,7 @@ export default function CursorInteraction({ children }) {
       }, 300);
     }
 
-    if (pageX === xCoordinate && pageY === yCoordinate) {
+    if (pageX === xCoordinate || pageY === yCoordinate) {
       setStaticCoords(true);
     }
 
@@ -48,55 +49,69 @@ export default function CursorInteraction({ children }) {
 
   useEffect(() => {
     const timeoutID = setTimeout(() => {
-      if (yCount >= Math.PI / 2) {
-        yCount = 0;
+      if (torque >= 5.25) {
+        setTorque(-1);
       } else {
-        yCount += 0.01;
+        setTorque(prevValue => prevValue + 0.01);
       }
-
-      setYOffset(Math.sin(yCount));
-    }, 10);
+    }, 3);
 
     return () => {
       clearTimeout(timeoutID);
     };
-  }, [yOffset]);
+  }, [torque]);
+
+  useEffect(() => {
+    function getStarOffset() {
+      const starOffsets = [];
+
+      for (let i = 0; i < starQuantity; i++) {
+        // if (i % 2 === 1) {
+        //   starOffsets.push(getRandomFloat());
+        // } else {
+        //   starOffsets.push(i);
+        // }
+
+        starOffsets.push(i);
+      }
+
+      setStarOffsets(starOffsets);
+    }
+
+    function getRandomFloat(min = 0, max = 5.26) {
+      return Math.random() * (max - min + 0.01) + min;
+    }
+
+    getStarOffset();
+  }, [starQuantity]);
 
   return (
     <div id={CANVAS_ID} className={s.canvas}>
       {children}
 
-      <img
-        src={star}
-        alt="star"
-        className={staticCoords ? s.activStar : s.star}
-        style={{ top: yCoordinate - 60, left: xCoordinate - 60 }}
-      />
-
-      <img
-        src={star}
-        alt="star"
-        className={staticCoords ? s.activStar : s.star}
-        style={{ top: yCoordinate - 60, left: xCoordinate + 30 }}
-      />
-
-      <img
-        src={star}
-        alt="star"
-        className={staticCoords ? s.activStar : s.star}
-        style={{ top: yCoordinate + 30, left: xCoordinate - 60 }}
-      />
-
-      <img
-        src={star}
-        alt="star"
-        className={staticCoords ? s.activStar : s.star}
-        style={{ top: yCoordinate + 30, left: xCoordinate + 30 }}
-      />
+      {starOffsets.length > 0 &&
+        starOffsets.map((offset, idx) => (
+          <img
+            key={offset + idx}
+            src={star}
+            alt="star"
+            className={s.star}
+            style={{
+              top:
+                yCoordinate +
+                CENTERING +
+                DIAMETER * Math.sin(staticCoords ? torque + offset : offset),
+              left:
+                xCoordinate +
+                CENTERING +
+                DIAMETER * Math.cos(staticCoords ? torque + offset : offset),
+            }}
+          />
+        ))}
     </div>
   );
 }
 
 CursorInteraction.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
