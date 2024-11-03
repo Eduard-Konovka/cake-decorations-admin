a09f32fd3b382a7fbeddb64f4f7a5166eba17dd8
Eduard Konovka <ed098ua@gmail.com>
2024-11-04 00:50:19 +0200

    Adds a trail

--- src/components/CursorInteraction/CursorInteraction.jsx
+++ src/components/CursorInteraction/CursorInteraction.jsx
@@ -1,34 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import star from 'assets/star.png';
 import s from './CursorInteraction.module.css';
 
-const CANVAS_ID = 'canvas';
-const CENTERING = -15;
-const DIAMETER = 90;
+const CONTAINER_ID = 'cursorInteractionContainer';
+const X_CENTERING = -10;
+const Y_CENTERING = -9;
+const colorsData = [
+  '#80ffff',
+  '#800000',
+  '#ff8080',
+  '#ff0000',
+
+  '#ff80ff',
+  '#008000',
+  '#80ff80',
+  '#00ff00',
+
+  '#ffff80',
+  '#000080',
+  '#8080ff',
+  '#0000ff',
+
+  '#000000',
+  '#808080',
+  '#ffffff',
+];
+let stopTimeout;
 
 export default function CursorInteraction({
-  starQuantity = 15,
+  diameter = 40,
+  starQuantity = 20,
+  starColorsData = colorsData,
   rotationSpeed = 3,
   plume = 300,
   children,
 }) {
-  const [canvas, setCanvas] = useState(null);
-  const [staticCoords, setStaticCoords] = useState(true);
+  const [container, setContainer] = useState(null);
+  const [isMovingOfMouse, setIsMovingOfMouse] = useState(false);
+  const [starColors, setStarColors] = useState([]);
   const [starOffsets, setStarOffsets] = useState([]);
+  const [ringThicknessOffsets, setRingThicknessOffsets] = useState([]);
   const [pageX, setPageX] = useState(null);
   const [pageY, setPageY] = useState(null);
   const [xCoordinate, setXCoordinate] = useState(null);
   const [yCoordinate, setYCoordinate] = useState(null);
   const [torque, setTorque] = useState(0);
+  const [trails, setTrails] = useState([]);
+
+  useEffect(() => setContainer(document.getElementById(CONTAINER_ID)), []);
 
   useEffect(() => {
-    setCanvas(document.getElementById(CANVAS_ID));
-  }, []);
+    const starOffsets = [];
+
+    for (let i = 0; i < starQuantity; i++) {
+      starOffsets.push(i);
+    }
+
+    setStarOffsets(starOffsets);
+  }, [starQuantity]);
+
+  useEffect(() => {
+    const ringOffsets = [];
+
+    for (let i = 0; i < starQuantity; i++) {
+      const offset = getRandomIntInclusive(0, diameter);
+
+      ringOffsets.push(offset);
+    }
+
+    setRingThicknessOffsets(ringOffsets);
+  }, [starQuantity, diameter]);
+
+  useEffect(() => {
+    const starColorsArr = [];
+
+    for (let i = 0; i < starQuantity; i++) {
+      const color = getRandomIntInclusive(0, starColorsData.length - 1);
+
+      starColorsArr.push(starColorsData[color]);
+    }
+
+    setStarColors(starColorsArr);
+  }, [starQuantity, starColorsData]);
 
   useEffect(() => {
     function handleMouseMove(e) {
-      setStaticCoords(false);
+      setIsMovingOfMouse(true);
 
       setPageX(e.pageX);
       setPageY(e.pageY);
@@ -37,28 +94,37 @@ export default function CursorInteraction({
         setXCoordinate(e.pageX);
         setYCoordinate(e.pageY);
       }, plume);
+
+      clearTimeout(stopTimeout);
+
+      stopTimeout = setTimeout(() => {
+        setIsMovingOfMouse(false);
+      }, plume + 100);
+
+      // FIXME
+      const newTrail = {
+        x: e.pageX,
+        y: e.pageY,
+        id: Date.now(),
+      };
+
+      setTrails(prevTrails => [...prevTrails, newTrail]);
     }
 
-    if (pageX === xCoordinate || pageY === yCoordinate) {
-      setStaticCoords(true);
-    }
-
-    canvas?.addEventListener('mousemove', handleMouseMove, { passive: true });
+    container?.addEventListener('mousemove', handleMouseMove, {
+      passive: true,
+    });
 
     return () => {
-      canvas?.removeEventListener('mousemove', handleMouseMove, {
+      container?.removeEventListener('mousemove', handleMouseMove, {
         passive: true,
       });
     };
-  }, [plume, canvas, pageX, pageY, xCoordinate, yCoordinate]);
+  }, [plume, container, pageX, pageY, xCoordinate, yCoordinate]);
 
   useEffect(() => {
     const timeoutID = setTimeout(() => {
-      if (torque >= 5.25) {
-        setTorque(-1);
-      } else {
-        setTorque(prevValue => prevValue + 0.01);
-      }
+      setTorque(prevValue => prevValue + 0.01);
     }, rotationSpeed);
 
     return () => {
@@ -67,58 +133,79 @@ export default function CursorInteraction({
   }, [rotationSpeed, torque]);
 
   useEffect(() => {
-    function getStarOffset() {
-      const starOffsets = [];
+    const intervalID = setInterval(() => {
+      // We remove old traces, leaving a maximum of 10 elements
+      setTrails(prevTrails => prevTrails.slice(-10));
+    }, 100);
 
-      for (let i = 0; i < starQuantity; i++) {
-        // if (i % 2 === 1) {
-        //   starOffsets.push(getRandomFloat());
-        // } else {
-        //   starOffsets.push(i);
-        // }
+    return () => clearInterval(intervalID);
+  }, []);
 
-        starOffsets.push(i);
-      }
-
-      setStarOffsets(starOffsets);
-    }
-
-    function getRandomFloat(min = 0, max = 5.26) {
-      return Math.random() * (max - min + 0.01) + min;
-    }
-
-    getStarOffset();
-  }, [starQuantity]);
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
   return (
-    <div id={CANVAS_ID} className={s.canvas}>
+    <div id={CONTAINER_ID} className={s.container}>
       {children}
+
+      {/* {trail.map(points =>
+        points.map(point => (
+          <svg
+            key={point.id}
+            className={s.star}
+            style={{
+              left: point.left - 10,
+              top: point.top - 10,
+              fill: point.fill,
+            }}
+          >
+            <use href={`${icons}#icon-star`}></use>
+          </svg>
+        )),
+      )} */}
 
       {starOffsets.length > 0 &&
         starOffsets.map((offset, idx) => (
-          <img
+          <div
             key={offset + idx}
-            src={star}
-            alt="star"
             className={s.star}
             style={{
-              top:
-                yCoordinate +
-                CENTERING +
-                DIAMETER * Math.sin(staticCoords ? torque + offset : offset),
               left:
                 xCoordinate +
-                CENTERING +
-                DIAMETER * Math.cos(staticCoords ? torque + offset : offset),
+                X_CENTERING +
+                (diameter + ringThicknessOffsets[idx]) *
+                  Math.cos(isMovingOfMouse ? offset : torque + offset),
+              top:
+                yCoordinate +
+                Y_CENTERING +
+                (diameter + ringThicknessOffsets[idx]) *
+                  Math.sin(isMovingOfMouse ? offset : torque + offset),
+              backgroundColor: starColors[idx],
             }}
           />
         ))}
+
+      {trails.map(trail => (
+        <div
+          key={trail.id}
+          className={s.trail}
+          style={{
+            left: trail.x - 10 + 'px',
+            top: trail.y - 10 + 'px',
+          }}
+        />
+      ))}
     </div>
   );
 }
 
 CursorInteraction.propTypes = {
+  diameter: PropTypes.number,
   starQuantity: PropTypes.number,
+  starColorsData: PropTypes.array,
   rotationSpeed: PropTypes.number,
   plume: PropTypes.number,
   children: PropTypes.node.isRequired,
